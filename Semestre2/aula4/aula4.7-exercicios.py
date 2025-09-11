3cd1efd2f9367410f7b3afa0d6643e464f116b23
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-10 22:32:22 -0300

    Aprendizado sobre dicionario

---
+++ Semestre2/aula4/aula4.7-exercicios.py
@@ -0,0 +1,35 @@
+frutas = {
+    'maçã': 'vermelho',
+    'banana': 'amarelo',
+    'uva': 'roxo'
+}
+
+#print(frutas['maçã'])
+#chave = input('Digite a chave : ')
+#valor = input('Digite o valor : ')
+#frutas[chave] = valor
+#print(frutas)
+#frutas['banana'] = input('Digite a nova cor : ')
+#print(frutas)
+#a = frutas.pop('maçã')
+#print(frutas)
+#for i in frutas.keys():
+#     print(i)
+#for i in frutas.values():
+#    print(i)
+#if 'banana' in frutas:
+#    print('Existe essa chave no dicionario')
+#else:
+#    print('None')
+
+frutab = {
+    'Kiwi': 'verde',
+    'morango': 'vermelho',
+    'uva': 'amarela'
+}
+#print(frutas)
+#print(frutab)
+#frutas.update(frutab)
+#print(frutas)
+
+print(len(frutas))
